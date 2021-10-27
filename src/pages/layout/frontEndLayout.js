a1f7dad943bc036170e16611f4dad5491cda7ac1
apabaad <apabaad@yahoo.com>
2021-10-27 11:18:33 +1100

    project initialization and setup

---
+++ src/pages/layout/frontEndLayout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../../components/Layout/Footer';
+import Header from '../../components/Layout/Header';
+
+const FrontEndLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default FrontEndLayout;
